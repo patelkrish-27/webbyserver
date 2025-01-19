094a08f1744c168afb28ccc248a4c2f0f155a569
PATEL KRISH <125797284+patelkrish-27@users.noreply.github.com>
2025-01-19 12:08:12 +0530

    Update mailSender.js
    changed the port of the mail sender

--- src/utils/mailSender.js
+++ src/utils/mailSender.js
@@ -4,6 +4,7 @@ const mailSender = async (email, title, body) => {
     // Create a Transporter to send emails
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
+      port:587,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
